fd90126c05991574bbea1d4224e6a67868c7b081
ammar <ammaraglan6@gmail.com>
2023-11-11 19:32:19 +0200

    Auth done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+import View.Authentication;
+
 public class Main {
     public static void main(String[] args) {
+        Authentication authentication = new Authentication();
+        authentication.authentictaionView();
     }
 }
---
+++ src/View/Authentication.java
@@ -0,0 +1,24 @@
+package View;
+
+import Controll.AuthenticationService;
+
+import java.util.Scanner;
+
+public class Authentication {
+    public void authentictaionView()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+        AuthenticationService authenticationService = new AuthenticationService();
+        while(!authenticationService.confirmAccount(username, password)){
+            System.out.print("Enter username again: ");
+            username = scanner.nextLine();
+            System.out.print("Enter password again: ");
+            password = scanner.nextLine();
+        }
+        System.out.print("hello");
+    }
+}
